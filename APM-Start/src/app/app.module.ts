6b000966599006fc776d5823ed28a6ea0078c87f
Heindrich Paul <heindrich.paul@gmail.com>
2018-10-09 14:24:22 +0200

    Checkpoint 1

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { StarComponent } from './shared/star.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ APM-Start/src/app/shared/convert-to-space.pipe.ts
@@ -0,0 +1,13 @@
+import { PipeTransform, Pipe } from '@angular/core';
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform {
+
+    transform(value: string, character: string) {
+        return value.replace(character, ' ');
+    }
+
+}
